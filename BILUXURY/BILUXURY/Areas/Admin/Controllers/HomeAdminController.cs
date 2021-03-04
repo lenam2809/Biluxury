2eb6b91ea7b1d38145a7ac708c126fa1e937619a
lenam2809 <49664798+lenam2809@users.noreply.github.com>
2021-03-04 21:47:21 +0700

    first commit

---
+++ BILUXURY/BILUXURY/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,71 @@
+﻿using BILUXURY.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BILUXURY.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult QLSanPham()
+        {
+            var sp = (from p in data.SANPHAMs
+                      select p).ToList();
+            ViewBag.listsp = sp;
+            return View();
+        }
+
+        public ActionResult AddSanPham()
+        {
+            return View();
+        }
+
+        public ActionResult QLNews()
+        {
+            return View();
+        }
+
+        public ActionResult QLNhanVien()
+        {
+            return View();
+        }
+
+        public ActionResult QLKhachHang()
+        {
+            return View();
+        }
+
+        public ActionResult QLDonHang()
+        {
+            return View();
+        }
+
+        //public ActionResult deleteProduct(int id = -1)
+        //{
+
+        //    var sp = data.SANPHAMs.Where(p => p.MaSP == id).FirstOrDefault();
+        //    if (sp != null)
+        //    {
+        //        data.SANPHAMs.DeleteOnSubmit(sp);
+        //        data.SANPHAMs.Sa;
+        //    }
+        //    return RedirectToAction("QLSanPham", "Admin");
+        //}
+    }
+}
---
+++ BILUXURY/BILUXURY/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using BILUXURY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BILUXURY.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            var sp = (from p in data.SANPHAMs
+                      select p).ToList();
+            ViewBag.listsp = sp;
+            return View();
+        }
+    }
+}
